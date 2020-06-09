dec1b72c6ebe3ed31883ce0606c9f81afe38ac03
zhangbei <zhangbei823@gmail.com>
2020-06-09 10:49:07 +0800

    Tree Traversals

--- DataStructure/BinaryTree/BinaryTree.go
+++ DataStructure/BinaryTree/BinaryTree.go
@@ -1,6 +1,9 @@
 package BinaryTree
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/zbml/DataStructureAndAlgorithm/DataStructure/Stack"
+)
 
 type Tree struct {
 	Value  int
@@ -19,6 +22,24 @@ func PreOrder(t *Tree) {
 	PreOrder(t.Right)
 }
 
+func PreOrder2(t *Tree) {
+	cur := t
+	stack := Stack.NewStack()
+	for {
+		fmt.Println(cur.Value)
+		if cur.Left != nil {
+			stack.Push(cur)
+			cur = cur.Left
+		} else {
+			if stack.IsEmpty() {
+				break
+			}
+			cTree, _ := stack.Pop()
+			cur = cTree.(*Tree).Right
+		}
+	}
+}
+
 func InOrder(t *Tree) {
 	if t == nil {
 		return
@@ -29,6 +50,26 @@ func InOrder(t *Tree) {
 	InOrder(t.Right)
 }
 
+func InOrder2(t *Tree) {
+	cur := t
+	s := Stack.NewStack()
+	for {
+		if cur.Left != nil {
+			s.Push(cur)
+			cur = cur.Left
+		} else {
+			fmt.Println(cur.Value)
+			if s.IsEmpty() {
+				break
+			}
+			t, _ := s.Pop()
+			cTree := t.(*Tree)
+			fmt.Println(cTree.Value)
+			cur = cTree.Right
+		}
+	}
+}
+
 func PostOrder(t *Tree) {
 	if t == nil {
 		return
@@ -37,4 +78,27 @@ func PostOrder(t *Tree) {
 	PostOrder(t.Left)
 	PostOrder(t.Right)
 	fmt.Println(t.Value)
+}
+
+func PostOrder2(t *Tree) {
+	cur := t
+	s := Stack.NewStack()
+	lastVisit := t
+
+	for cur != nil || !s.IsEmpty() {
+		for cur != nil {
+			s.Push(cur)
+			cur = cur.Left
+		}
+		node, _ := s.Peek()
+		cur = node.(*Tree)
+		if cur.Right == nil || cur.Right == lastVisit {
+			fmt.Println(cur.Value)
+			s.Pop()
+			lastVisit = cur
+			cur = nil
+		} else {
+			cur = cur.Right
+		}
+	}
 }
--- DataStructure/BinaryTree/BinaryTree_test.go
+++ DataStructure/BinaryTree/BinaryTree_test.go
@@ -8,7 +8,7 @@ package BinaryTree
 //      / \
 //     8   9
 
-func initTree() *Tree {
+func initTree1() *Tree {
 	var node9 = &Tree{
 		Value: 9,
 	}
@@ -49,9 +49,55 @@ func initTree() *Tree {
 	return root
 }
 
-func ExamplePreOrder() {
-	tree := initTree()
-	PreOrder(tree)
+//   1
+//  / \
+// 2  3
+// /   \
+//4     5
+// \
+//  6
+// /  \
+//7    8
+func initTree2() *Tree {
+	var node7 = &Tree{
+		Value: 7,
+	}
+	var node8 = &Tree{
+		Value: 8,
+	}
+	var node6 = &Tree{
+		Value: 6,
+		Left:  node7,
+		Right: node8,
+	}
+	var node4 = &Tree{
+		Value: 4,
+		Right: node6,
+	}
+	var node5 = &Tree{
+		Value: 5,
+	}
+	var node2 = &Tree{
+		Value: 2,
+		Left:  node4,
+	}
+	var node3 = &Tree{
+		Value: 3,
+		Right: node5,
+	}
+	var node1 = &Tree{
+		Value:  1,
+		Left:   node2,
+		Right:  node3,
+		IsRoot: true,
+	}
+
+	return node1
+}
+
+func ExamplePreOrderTree1() {
+	tree1 := initTree1()
+	PreOrder(tree1)
 	//output: 1
 	//2
 	//4
@@ -63,8 +109,48 @@ func ExamplePreOrder() {
 	//7
 }
 
+func ExamplePreOrderTree2() {
+	tree2 := initTree2()
+	PreOrder(tree2)
+	//output: 1
+	//2
+	//4
+	//6
+	//7
+	//8
+	//3
+	//5
+}
+
+func ExamplePreOrder2Tree1() {
+	tree := initTree1()
+	PreOrder2(tree)
+	//output: 1
+	//2
+	//4
+	//5
+	//3
+	//6
+	//8
+	//9
+	//7
+}
+
+func ExamplePreOrder2Tree2() {
+	tree2 := initTree2()
+	PreOrder2(tree2)
+	//output: 1
+	//2
+	//4
+	//6
+	//7
+	//8
+	//3
+	//5
+}
+
 func ExampleInOrder() {
-	tree := initTree()
+	tree := initTree1()
 	InOrder(tree)
 	//output: 4
 	//2
@@ -77,9 +163,37 @@ func ExampleInOrder() {
 	//7
 }
 
+func ExampleInOrder2() {
+	tree := initTree1()
+	InOrder2(tree)
+	//output: 4
+	//2
+	//5
+	//1
+	//8
+	//6
+	//9
+	//3
+	//7
+}
+
 func ExamplePostOrder() {
-	tree := initTree()
+	tree := initTree1()
 	PostOrder(tree)
+	// Output: 4
+	//5
+	//2
+	//8
+	//9
+	//6
+	//7
+	//3
+	//1
+}
+
+func ExamplePostOrder2() {
+	tree := initTree1()
+	PostOrder2(tree)
 	// Output: 4
 	//5
 	//2
